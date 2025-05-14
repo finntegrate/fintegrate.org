9b1af8012bd0800b4a9f1885b313a6a7b1ef9467
Brylie Christopher Oxley <brylie@protonmail.com>
2025-05-14 13:37:56 +0300

    chore: add and configure astro-icon dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://finntegrate.org',
+
   vite: {
     plugins: [ tailwindcss() ]
-  }
+  },
+
+  integrations: [icon()]
 });
